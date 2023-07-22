43784f2c215a18b35ebf7c0712ec206c48a7ae19
jaehyeonlee <jerry0608s@gmail.com>
2023-07-22 14:19:25 +0900

    docs : sec2, 3, 4, 5, 6, footer 영역 style2.css 및 js 작업
    대략적인 뼈대작업중...
    
    sec4__left-slider 영역 swiper 작업필요

--- js/swiper.js
+++ js/swiper.js
@@ -1,35 +1,101 @@
+function createSwiper(container, slidesPerView, spaceBetween, autoplayDelay = 5000, vertical = false) {
+    return new Swiper(container, {
+        slidesPerView: slidesPerView,
+        effect: 'slide',
+        centeredSlides: true,
+        loop: true,
+        spaceBetween: spaceBetween,
+        autoplay: {
+            delay: autoplayDelay,
+            disableOnInteraction: false
+        },
+        allowTouchMove: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        direction: vertical ? 'vertical' : 'horizontal',
+    });
+}
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1, // 몇 개 보이게 할것인지
-    effect: 'slide',
-    centeredSlides: true,
-    loopAdditionalSlides : 0,
-    loopFillGroupWithBlank: false,
-    spaceBetween: 20, // 각 요소 간 간격
-    autoplay: {     // 자동슬라이드 (false-비활성화)
-        delay: 5000, // 시간 설정
-        disableOnInteraction: false
-    },
-    loop: true,
-    allowTouchMove: false, // (false-스와이핑안됨)버튼으로만 슬라이드 조작이 가능
+var swiper1 = createSwiper(".mySwiper", 1, 20);
+var swiper2 = createSwiper(".mySwiper2", 1, 50, 5000, false);
+var swiper3 = createSwiper(".mySwiper3", 2, 0, 5000, true);
+var swiper4 = createSwiper(".mySwiper4", 1, 50, 3000);
 
-    // breakpoints : {
-    //     1600 : {
-    //         slidesPerView: 5,
-    //         spaceBetween: 50,
-    //     },
-    //     1300 : {
-    //         slidesPerView: 4,
-    //         spaceBetween: 40,
-    //     },
-    //     1080 : {
-    //         slidesPerView: 3,
-    //         spaceBetween: 30,
-    //     },
-    //     768 : {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20,
-    //     },
-    //
-    // }
-});
+// breakpoints를 사용하려면 아래와 같이 추가하면 됩니다.
+// swiper1.params.breakpoints = {
+//     1600 : {
+//         slidesPerView: 5,
+//         spaceBetween: 50,
+//     },
+//     1300 : {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//     },
+//     1080 : {
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//     },
+//     768 : {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//     },
+// };
+
+// swiper2.params.breakpoints = {
+//     1600 : {
+//         slidesPerView: 5,
+//         spaceBetween: 50,
+//     },
+//     1300 : {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//     },
+//     1080 : {
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//     },
+//     768 : {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//     },
+// };
+
+// swiper3.params.breakpoints = {
+//     1600 : {
+//         slidesPerView: 5,
+//         spaceBetween: 50,
+//     },
+//     1300 : {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//     },
+//     1080 : {
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//     },
+//     768 : {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//     },
+// };
+
+// swiper4.params.breakpoints = {
+//     1600 : {
+//         slidesPerView: 5,
+//         spaceBetween: 50,
+//     },
+//     1300 : {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//     },
+//     1080 : {
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//     },
+//     768 : {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//     },
+// };
