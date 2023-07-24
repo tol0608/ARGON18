9aec7ee4c37129310cd67ca6ca06bcb1c08d512b
jaehyeonlee <jerry0608s@gmail.com>
2023-07-24 19:53:21 +0900

    docs : 전체 영역 style2.css 및 js 작업
    sec3까지 기본 반응형 작업 완료,
    
    이후 기본 반응형 작업 후,
    
    sec2 이벤트리스너, sec3 버튼 위치, sec4__left-slider 영역 이벤트리스너 및 반응형 작업 필요

--- js/main.js
+++ js/main.js
@@ -27,4 +27,29 @@ document.addEventListener('DOMContentLoaded', function () {
     removeBox.addEventListener('click', function () {
         document.querySelector('.header__search-on').classList.remove('header__search-on');
     });
+
 });
+
+function addOrRemoveResponsiveClass() {
+    // 미디어 쿼리를 정의합니다. 원하는 조건에 따라 수정할 수 있습니다.
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+    // 뷰포트 크기에 따라 클래스를 추가 또는 제거합니다.
+    const on = document.querySelector(".sec2__description");
+    const off = document.querySelector(".sec2__description--off");
+
+    if (mediaQuery.matches) {
+        on.style.display = "none";
+        off.style.display = "block";
+    } else {
+        on.style.display = "block";
+        off.style.display = "none";
+
+    }
+}
+
+// 페이지 로드 시 클래스를 추가 또는 제거합니다.
+addOrRemoveResponsiveClass();
+
+// 뷰포트 크기가 변경될 때마다 클래스를 추가 또는 제거합니다.
+window.addEventListener("resize", addOrRemoveResponsiveClass);
